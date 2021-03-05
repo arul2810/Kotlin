b523c4b64986950f478e08ebf1bdc0054a0a27b3
Arul Prakash Samathuvamani <arul2810@gmail.com>
2021-03-05 10:46:52 +0530

    Completed Problem A - Remove Duplicates

---
+++ Kotlin Heros Practice 6/Remove_Duplicates.kt
@@ -0,0 +1,46 @@
+
+
+import java.util.ArrayList// Created by Arul Prakash Samathuvamani - University of Leeds PGT - hello@arulprakash.dev
+
+// Created for CodeForces Kotlin Heros Practice 6 Contest - Problem A - Remove Duplicates
+
+private fun readLn() = readLine()!! // string line
+private fun readInt() = readLn().toInt() // single int
+private fun readStrings() = readLn().split(" ") // list of strings
+private fun readInts() = readStrings().map { it.toInt() } // list of ints
+
+fun main(){
+
+// num_var -> Number of Variables in Array
+
+var num_var = readLine()!!.toInt()
+
+var array  = ArrayList(readStrings().map { it.toInt() })
+
+var unique = num_var
+
+// Finding the duplicates and removing left element
+
+for ( i in 0..num_var-1){
+
+ for( j in i+1..num_var-1){
+     if(array[i] == array[j]){
+         array[i] = 0
+        unique -= 1
+     }
+ }
+
+}
+
+// Printing Output
+
+println(unique)
+
+for(i in 0..num_var-1){
+
+if(array[i] !=0){
+    print(array[i])
+    print(" ")
+}
+}
+}
